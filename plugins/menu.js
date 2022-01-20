e3af9783bee7f41ee45a8154defb2e7484270bdf
TEDZO123 <94947774+TEDZO123@users.noreply.github.com>
2022-01-20 21:49:35 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -10,139 +10,140 @@ Asena.addCommand({pattern: 'menu', fromMe: false, desc: Lang.WP}, (async (messag
     var r_text = new Array ();
     
     
-    r_text[0] = "https://i.imgur.com/vUdEZMv.jpg";
+    r_text[0] = "https://i.imgur.com/1pOfIIf.jpeg";
     
     
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `⛦━━━✨️*QUEEN ANGELLA*✨️━━━⛦
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `⛦━━━━━💝TEDZO SIR BOT💝━━━━━⛦
 
 
-𝐇𝐞𝐥𝐥𝐨👋 𝐈 𝐚𝐦 𝐚 QUEEN ANGELLA 𝐛𝐨𝐭.
-            *ANGELLA*
+𝐇𝐞𝐥𝐥𝐨👋 𝐈 𝐚𝐦 asluser 𝐛𝐨𝐭.
+            *⫷⫸ᴀsʟᴜ sᴇʀ ʙᴏᴛ⫷⫸*
+const Asena = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
 
-■□■□■□■□■□■□■□■□■□■□
-♕ *𝙈𝙚𝙙𝙞𝙖 𝘾𝙤𝙢𝙢𝙖𝙣𝙙𝙨* ♕
+const Language = require('../language');
+const Lang = Language.getString('wallpaper');
 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -song <text>
-🍙𝙐𝙨𝙚➜ Downloads song for you.
-⚠️.song  baby love
+Asena.addCommand({pattern: 'menu', fromMe: false, desc: Lang.WP}, (async (message, match) => {
 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -video <yt link>
-🍙𝙐𝙨𝙚➜ Downloads video from YouTube link.
+    var r_text = new Array ();
+    
+    
+    r_text[0] = "https://i.imgur.com/1pOfIIf.jpeg";
+    
+    
+    var i = Math.floor(1*Math.random())
 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -insta <link>
-🍙𝙐𝙨𝙚➜   Downloads content from instagram.
+    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -yt<text>
-🍙𝙐𝙨𝙚➜   Gives you YT links.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -show <show name>
-🍙𝙐𝙨𝙚➜   Get info related to tv series and shows.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -gif 
-🍙𝙐𝙨𝙚➜ Converts video to gif.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -tomp3 
-🍙𝙐𝙨𝙚➜ Converts video into audio.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -say <text>
-🍙𝙐𝙨𝙚➜ Converts text into voice.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -img <text>
-🍙𝙐𝙨𝙚➜ It sends image from google.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -trt <language code>
-🍙𝙐𝙨𝙚➜ Translate the text you tag.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -wiki <text>
-🍙𝙐𝙨𝙚➜ It sends Wikipedia result.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -lyric <text>
-🍙𝙐𝙨𝙚➜ Finds the lyrics of the song.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -covid <country code>
-🍙𝙐𝙨𝙚➜ Send the covid stats of your country.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -weather <city>
-🍙𝙐𝙨𝙚➜ Tells you about the weather of your place.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -removebg 
-🍙𝙐𝙨𝙚➜ Removes the background of tge image.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -ocr
-🍙𝙐𝙨𝙚➜ Finds the text written on the image.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -wallpaper
-🍙𝙐𝙨𝙚➜ It sends you random wallpaper.
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `⛦━━━━━💝TEDZO SIR BOT💝━━━━━⛦
 
 
-■□■□■□■□■□ANGELLA■□■□■□■□■□
+𝐇𝐞𝐥𝐥𝐨👋 𝐈 𝐚𝐦 asluser 𝐛𝐨𝐭.
+            *⫷⫸ᴀsʟᴜ sᴇʀ ʙᴏᴛ⫷⫸*
 
+╭────────────────╮
+│  *ASLUSIR MENU*
+│
+╰────────────────╯
+╭────────────────╮
+   TEDZO SIR BOT
+╰────────────────╯
+🍂 ɢʀᴏᴜᴘ ᴄᴏᴍᴍᴀɴᴅs
+╭────────────────
+│ 🎻 ᴀᴅᴅ
+│ 🎻 ᴋɪᴄᴋ
+│ 🎻 ᴍᴜᴛᴇ
+│ 🎻 ᴜɴᴍᴜᴛᴇ
+│ 🎻 ᴘʀᴏᴍᴏᴛᴇ
+│ 🎻 ᴅᴇᴍᴏᴛᴇ
+│ 🎻 ɪɴᴠɪᴛᴇ
+│ 🎻 ʀᴇᴠᴏᴋᴇ
+│ 🎻 ᴊᴏɪɴ
+│ 🎻 ᴡᴇʟᴄᴏᴍᴇ
+│ 🎻 ɢᴏᴏᴅʙʏᴇ
+│ 🎻 ʙᴀɴʙʏᴇ
+│ 🎻 ʟᴇғᴛ
+│ 🎻 ᴡᴀʀɴ
+│ 🎻 ᴠᴏᴛᴇ
+│ 🎻 ᴛᴀɢ
+╰────────────────
+  📤 ᴅᴏᴡɴʟᴏᴅ ᴄᴏᴍᴍᴀɴᴅs
+╭────────────────
+│ 🎭 ʏᴛᴠ
+│ 🎭 ʏᴛs
+│ 🎭 sᴏɴɢ
+│ 🎭 ᴠɪᴅᴇᴏ
+│ 🎭 ɪɴsᴛᴀ
+│ 🎭 sᴛᴏʀʏ
+│ 🎭 ғʙ
+│ 🎭 ɪᴍɢ
+│ 🎭 ᴛᴡɪᴛᴛᴇʀ
+│ 🎭 ᴘɪɴᴛʀᴇsᴛ
+│ 🎭 ᴍᴇᴅɪᴀғɪʀᴇ
+╰────────────────
+🌿 ᴍɪsᴄ ᴄᴏᴍᴍᴀɴᴅs
+╭────────────────
+│ 🍒 ᴡᴀsᴛᴇᴅ
+│ 🍎 ᴛʀɪɢɢᴇᴅ
+│ 🍒 ʜɪsᴛᴏ
+│ 🍎 ᴠᴇᴄᴛᴏʀ
+│ 🍒 ᴀᴠᴇᴄ
+│ 🍎 ᴍᴘ3
+│ 🍒 sᴛɪᴄᴋᴇʀ
+│ 🍎 ᴘʜᴏᴛᴏ
+│ 🍒 ʀᴇᴠᴇʀᴄᴇ
+│ 🍎 ᴄᴜᴛ
+│ 🍒 ᴛʀɪᴍ
+│ 🍎 ʀᴏᴛᴀᴛᴇ
+│ 🍒 ᴍᴇʀɢᴇ
+│ 🍎 ᴄᴏᴍᴘʀᴇss
+│ 🍒 menu
+│ 🍎 ᴛʀᴇʙʟᴇ
+│ 🍒 ᴘɪᴛᴄʜ
+│ 🍎 ʟᴏᴡ
+│ 🍒 ᴛᴛs
+│ 🍎 ᴜɴᴠᴏɪᴄᴇ
+│ 🍒 ᴠᴏɪᴄᴇ
+│ 🍎 ʀᴇᴍᴏᴠᴇʙɢ
+│ 🍒 ᴀᴛᴛᴘ
+│ 🍎 ᴇᴍᴏᴊɪ
+│ 🍒 ғɪɴᴅ
+│ 🍎 ᴛʀᴛ
+│ 🍒 ᴘʟᴜɢɪɴ
+│ 🍎 ᴘʟᴜɢɪɴ ʟɪsᴛ
+│ 🍒 ʀᴇᴍᴏᴠᴇ
+│ 🍎 ғɪʟᴛᴇʀ
+│ 🍒 ᴊɪᴅ
+│ 🍎 ʙʟᴏᴄᴋ
+│ 🍒 ᴜɴʙʟᴏᴄᴋ
+│ 🍎 sᴇᴛᴀʙᴏᴜᴛ
+│ 🍒 sᴇᴛsᴛᴀᴛᴜs
+│ 🍎 ᴛᴀᴋᴇ
+│ 🍒 ʙʀᴏᴀᴅᴄᴀsᴛ
+│ 🍎 ᴜʀʟ
+│ 🍒 ᴡʜᴏɪs
+│ 🍎 sʜᴇᴅᴜʟᴇ
+│ 🍒 ᴘɪɴɢ
+╰────────────────
+💢 ʜᴇʀᴏᴋᴜ ᴄᴏᴍᴍᴀɴᴅs
+╭────────────────
+│ 📍 ʀᴇsᴛᴀʀᴛ
+│ 📌 sʜᴜᴛᴅᴏᴡɴ
+│ 📍 sᴇᴛᴠᴀʀ
+│ 📌 ɢᴇᴛᴠᴀʀ
+│ 📍 ᴀʟʟᴠᴀʀ
+│ 📌 ᴜᴘᴅᴀᴛᴇ
+│ 📍 ᴜᴘᴅᴀᴛᴇ ɴᴏᴡ
+╰────────────────
+`}) 
 
-♟ *𝙁𝙪𝙣 𝘾𝙤𝙢𝙢𝙖𝙣𝙙𝙨* ♟
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -joke 
-🍙𝙐𝙨𝙚➜ It sends a random joke.
- 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -meme <text>
-🍙𝙐𝙨𝙚➜ Cations the image into a meme.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -quote 
-🍙𝙐𝙨𝙚➜ It sends a random quote.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -ss <website link>
-🍙𝙐𝙨𝙚➜ It sends the screenshot of the website.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -changesay <text>
-🍙𝙐𝙨𝙚➜ Converts text into changesay meme image.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -trumpsay
-🍙𝙐𝙨𝙚➜ Convert text into Trump's tweet.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -compliment 
-🍙𝙐𝙨𝙚➜ Gives you a compliment.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -bitly <link>
-🍙𝙐𝙨𝙚➜   Shorten your link.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -dict 
-🍙𝙐𝙨𝙚➜   Dictionary [-dict en;anime]
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -zodiac <leo> 
-🍙𝙐𝙨𝙚➜   Tells you about your horoscope.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -qr <text>
-🍙𝙐𝙨𝙚➜   Converts text into qr code.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -movie 
-🍙𝙐𝙨𝙚➜  Gives you info about movie.
-⚠️movie master
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -anime <text>
-🍙𝙐𝙨𝙚➜  Gives you info about anime.
-⚠️anime angella
-
-
-■□■□■□■□■ANGELLA□■□■□■□■□■□
-
-
-♝ *𝙎𝙩𝙞𝙘𝙠𝙚𝙧 𝘾𝙤𝙢𝙢𝙖𝙣𝙙𝙨* ♝
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -sticker 
-🍙𝙐𝙨𝙚➜ Converts img/gif into a sticker.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -photo 
-🍙𝙐𝙨𝙚➜ Converts sticker into image.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -attp <text>
-🍙𝙐𝙨𝙚➜ Converts text into glowing sticker.
-⚠️ex  attp Angella
-■□■□■□■□■□■□■□■□■□■□
-═════💢ANGELLA💢═════
-▣▣▣▣▣▣dripsmemes▣▣▣▣▣▣
+}));
 
 `}) 
 
